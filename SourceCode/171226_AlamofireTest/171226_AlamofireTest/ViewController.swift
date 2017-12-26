ba970ebefad44bfc4ba03baae85119cd4f070b23
simajune <tejayjung@gmail.com>
2017-12-26 17:06:57 +0900

    Generate Alamofire Project

---
+++ SourceCode/171226_AlamofireTest/171226_AlamofireTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  171226_AlamofireTest
+//
+//  Created by SIMA on 2017. 12. 26..
+//  Copyright © 2017년 SIMA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
