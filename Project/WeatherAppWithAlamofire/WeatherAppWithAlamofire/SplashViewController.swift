973d3348ad3e6efebea93e004088377547f4e4b5
simajune <tejayjung@gmail.com>
2018-01-03 17:10:38 +0900

    Add MainViewController, SplashViewController

---
+++ Project/WeatherAppWithAlamofire/WeatherAppWithAlamofire/SplashViewController.swift
@@ -0,0 +1,15 @@
+
+import UIKit
+import CoreLocation
+
+class SplashViewController: UIViewController, CLLocationManagerDelegate {
+
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let API_KEY = "89e4a79cf2b99a9f6ac5d5654d83a51b"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
